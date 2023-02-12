e12acbff32dc4b7e4d253f0acd2cdede3da727db
Aksanti Bahiga tacite <71480535+marcellintacite@users.noreply.github.com>
2023-02-12 18:29:36 +0300

    New features

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import GlobalStyle from "./theme/globalStyles";
 import Accueil from "./pages/Accueil/Accueil";
+import HopitalProche from "./pages/HopitalProche/HopitalProche";
 import QuickTest from "./pages/Quicktest/QuickTest";
 import Navbar from "./components/Navbar";
 import ScrollToTop from "./fonctions/ScrollToTop";
+import Map from "./pages/map/Map";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/Quicktest" element={<QuickTest />} />
+        <Route path="/hopitalProche" element={<HopitalProche />}></Route>
+        <Route path="hopitalProche/map" element={<Map />} />
+        <Route path="*" element={<Accueil />} />
       </Routes>
     </div>
   );
